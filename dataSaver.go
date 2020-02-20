8c62fec8fcb5ddcf9a443dc513684217727ddbd2
CoffeBee <40368492+CoffeBee@users.noreply.github.com>
2020-02-20 11:40:24 +0300

    Add form

--- dataSaver.go
+++ dataSaver.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
 )
 
 type Request struct {
-	a int
-	b int
-	c int
+	a  int
+	b  int
+	c  int
+	id int
 }
 
+func requestToServer(reqq Request, wg *sync.WaitGroup) {
+	defer wg.Done()
+	resp, err := http.PostForm("http://172.20.10.3:8080", url.Values{"a": {strconv.Itoa(reqq.a)}, "b": {strconv.Itoa(reqq.b)}, "c": {strconv.Itoa(reqq.c)}, "id": {strconv.Itoa(reqq.id)}})
+	fmt.Print(resp, err)
+}
 func runServer(addr string) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
 	mux := http.NewServeMux()
@@ -21,6 +31,19 @@ func runServer(addr string) {
 				tmpl.Execute(w, nil)
 				return
 			}
+			var wg sync.WaitGroup
+			a, erra := strconv.Atoi(r.FormValue("a"))
+			b, errb := strconv.Atoi(r.FormValue("b"))
+			c, errc := strconv.Atoi(r.FormValue("c"))
+			if erra == nil && errb == nil && errc == nil {
+				wg.Add(1)
+				reqq := Request{a: a, b: b, c: c}
+				_ = reqq
+				fmt.Println(a, b, c)
+				fmt.Print(rand.Intn(100))
+				go requestToServer(reqq, wg)
+			}
+			wg.Wait()
 		})
 
 	server := http.Server{
